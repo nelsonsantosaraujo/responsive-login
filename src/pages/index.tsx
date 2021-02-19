9c909df93aa579e44b80a797aa35b231d1f4a486
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-19 10:19:41 -0300

    feat: Add Api call and create input validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,65 @@
-import { Head } from 'next/document';
-import React from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
+import { useRouter } from 'next/router'
+import { Container, Background, Content, Form, FormGroup } from '../styles/pages/Home';
+import { FiX } from 'react-icons/fi';
 
-import { Container, Background, Content, Form } from '../styles/pages/Home';
+
+import api from '../services/api';
+
+interface formData {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  token: string;
+  name: string;
+  email: string;
+}
 
 const Home: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+
+  const history = useRouter();
+
+  const handleSubmit = useCallback( async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    var emailPattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+    if(!emailPattern.test(email)) {
+      setEmailIsValid(false);
+      return;
+    } else {
+      setEmailIsValid(true);
+    }
+
+    if(!password) {
+      setPasswordIsValid(false);
+      return;
+    } else {
+      setPasswordIsValid(true);
+    }
+
+    const formData: formData = {
+      email,
+      password,
+    };
+
+    try {
+      const response: Response = await api.post('users', formData);
+      console.log(response);
+      // history.push('/dashboard');
+
+    } catch(err) {
+      console.log(err);
+    }
+
+  }, [email, password]);
+
   return (
     <>
     <Container>
@@ -14,17 +70,41 @@ const Home: React.FC = () => {
             Olá, seja 
             bem-vindo!
           </h2>
-          <p>
+          <h4>
             Para acessar a plataforma,
             faça seu login.
-          </p>
-          <Form>
+          </h4>
+          <Form onSubmit={handleSubmit}>
             <label htmlFor="email">E-MAIL</label>
-            <input type="email" className="error" id="email" placeholder="user.name@mail.com" />
-            <div>Digite um e-mail válido.</div>
+            <FormGroup id="input_email" isValid={emailIsValid}>
+              <div>
+                <input 
+                  type="email" 
+                  className="error" 
+                  id="email" 
+                  placeholder="user.name@mail.com"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <FiX size={16} color="#FF377F" />
+              </div>
+              <p>Digite um e-mail válido.</p>
+            </FormGroup>
 
             <label htmlFor="password">SENHA</label>
-            <input type="password" id="password" placeholder="*******" />
+            <FormGroup id="input_password" isValid={passwordIsValid}>
+              <div>
+                <input 
+                  type="password" 
+                  id="password" 
+                  placeholder="*******"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <FiX size={16} color="#FF377F" />
+              </div>
+              <p>Digite sua senha.</p>
+            </FormGroup>
 
             <button type="submit">ENTRAR</button>
           </Form>
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 // import BackgroundLogin from '../../assets/background-login.png';
 import BackgroundLogin from '../../assets/background-image.png';
+
+interface FormGroupProps {
+  id: string;
+  isValid: boolean;
+}
 
 export const Container = styled.div`
   height: 100vh;
@@ -35,7 +40,7 @@ export const Content = styled.div`
       color: #383E71;
       }
 
-      p {
+      h4 {
         font-weight: 16px;
         line-height: 20px;
         font-weight: 600;
@@ -77,32 +82,6 @@ export const Form = styled.form`
     margin-top: 16px;
   }
 
-  input {
-    width: 100%;
-
-    font-size: 10px;
-    color: #989FDB; 
-
-    padding: 16px;
-    margin-top: 8px;
-    border: 1px solid #989FDB;
-    border-radius: 8px;
-
-    &::placeholder {
-      color: #989FDB;
-    }
-
-    &.error {
-      border-color: #FF377F;
-    }
-  }
-
-  div {
-    margin: 8px 16px 0;
-    font-size: 10px;
-    color: #FF377F;
-  }
-
   button {
     margin-top: 24px;
     padding: 14px 0;
@@ -116,5 +95,80 @@ export const Form = styled.form`
 
     background: linear-gradient(267.79deg, #383E71 0%, #9D25B0 99.18%);
     box-shadow: 0 10px 25px 0 #CF99DB;
+  }
+`;
+
+export const FormGroup = styled.div<FormGroupProps>`
+  div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 8px;
+    border: 1px solid #989FDB;
+    border-radius: 8px;
+
+    ${ props =>
+        !props.isValid && css`
+          border-color: #FF377F;
+        `
+    }
+  }
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    padding: 16px;
+
+    font-size: 10px;
+    color: #989FDB; 
+
+    &::placeholder {
+      color: #989FDB;
+    }
+
+    /* Remove estilo do autocomplete */
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus {
+      border: 0;
+      -webkit-text-fill-color: #989FDB;
+      -webkit-box-shadow: 0 0 0px 1000px transparent inset;
+      transition: background-color 5000s ease-in-out 0s;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+    visibility: hidden;
+    display: none;
+    ${ props =>
+        !props.isValid && css`
+          visibility: visible;
+          display: block;
+        `
+    }
+  }
+
+  p {
+    margin: 8px 16px 0;
+    font-size: 10px;
+    color: #FF377F ;
+    visibility: hidden;
+    display: none;
+    ${ props =>
+        !props.isValid && css`
+          visibility: visible;
+          display: block;
+        `
+    }
   }
 `;
