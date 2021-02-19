ae54e8858a04291f7ff3f59d10e62a9d12535d10
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-19 12:13:03 -0300

    feat: create simple Forgot Password page

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ForgotPassword: React.FC = () => {
+
+  return (
+    <>
+      <h1>
+        Forgot Password
+      </h1>
+    </>
+  )
+}
+
+export default ForgotPassword;
